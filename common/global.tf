6274bdf5846f23512c44364b835bc2f709e53f24
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-04 23:07:08 -0700

    make remote uses output .s3_remote_state

--- common/global.tf
+++ common/global.tf
@@ -5,3 +5,7 @@ data "terraform_remote_state" "global" {
     path = "${path.module}/../global/.terraform/terraform.tfstate"
   }
 }
+
+output "s3_remote_state" {
+  value = "${data.terraform_remote_state.global.s3_remote_state}"
+}
