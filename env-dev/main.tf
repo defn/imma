d57d085e0c86b7c0cad4f59c65b0369d7c79c09c
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-05 01:15:39 -0700

    configure network

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -3,4 +3,8 @@ module "env" {
   global_remote_state = "${var.global_remote_state}"
   env_name            = "${var.env_name}"
   env_cidr            = "${var.env_cidr}"
+  az_count            = "${var.az_count}"
+  az_names            = ["${var.az_names}"]
+  nat_bits            = "${var.nat_bits}"
+  nat_nets            = ["${var.nat_nets}"]
 }
--- env-dev/terraform.tfvars
+++ env-dev/terraform.tfvars
@@ -2,4 +2,10 @@ global_remote_state = "../global/.terraform/terraform.tfstate"
 
 env_name = "dev"
 
+az_count = "2"
+
+az_names = ["us-west-1b", "us-west-1c"]
+
 env_cidr = "172.20.0.0/16"
+
+nat_nets = [10, 11]
