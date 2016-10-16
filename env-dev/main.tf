1d37545f4b57b115fe1c23a07c9b2721c84c2aeb
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-15 23:42:26 -0700

    making common nets

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -6,4 +6,6 @@ module "env" {
   az_count            = "${var.az_count}"
   nat_bits            = "${var.nat_bits}"
   nat_nets            = ["${var.nat_nets}"]
+  common_bits         = "${var.common_bits}"
+  common_nets         = ["${var.common_nets}"]
 }
--- env-dev/terraform.tfvars
+++ env-dev/terraform.tfvars
@@ -7,3 +7,5 @@ az_count = "3"
 env_cidr = "172.20.0.0/16"
 
 nat_nets = [10, 11, 12]
+
+common_nets = [2, 3, 4]
