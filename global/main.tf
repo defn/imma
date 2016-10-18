05351464115bba8bd64947b44e8d3eabf91ae628
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-17 19:43:41 -0700

    use global to store env cidr, system networks

--- global/main.tf
+++ global/main.tf
@@ -1,6 +1,29 @@
+variable env_cidr {
+  default = {
+    "sandbox" = "172.16.0.0/16"
+    "admin"   = "172.19.0.0/16"
+    "dev"     = "172.20.0.0/16"
+  }
+}
+
+variable sys_nets {
+  default = {
+    common = [2, 3, 4, 5]
+    nat    = [10, 11, 12, 13]
+  }
+}
+
 module "global" {
   source          = "../fogg/global"
   aws_account_id  = "${var.aws_account_id}"
   domain_name     = "${var.domain_name}"
   s3_remote_state = "${var.s3_remote_state}"
+}
+
+output env_cidr {
+  value = "${var.env_cidr}"
+}
+
+output sys_nets {
+  value = "${var.sys_nets}"
 }
