e8c654ae8db8dd7a0429d1bdfa9d1d27f3f30c83
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-17 19:58:32 -0700

    env tf is common

--- env-dev/main.tf
+++
@@ -1,9 +0,0 @@
-module "env" {
-  source              = "../fogg/env"
-  global_remote_state = "${var.global_remote_state}"
-  env_name            = "${var.env_name}"
-  env_cidr            = "${data.terraform_remote_state.global.env_cidr[var.env_name]}"
-  az_count            = "${var.az_count}"
-  nat_nets            = ["${data.terraform_remote_state.global.sys_nets["nat"]}"]
-  common_nets         = ["${data.terraform_remote_state.global.sys_nets["common"]}"]
-}
