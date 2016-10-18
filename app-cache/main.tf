d602732b9d6a322061e109568706327bb5fe69a2
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-17 19:26:23 -0700

    enable dns service again

--- app-cache/main.tf
+++ app-cache/main.tf
@@ -58,29 +58,27 @@ module "nexus" {
   user_data           = "${var.user_data}"
 }
 
-#module "dns" {
+module "dns" {
+  source = "../../fogg/service"
 
-#  source              = "../../fogg/service"
+  global_remote_state = "${var.global_remote_state}"
 
-#  global_remote_state = "${var.global_remote_state}"
+  env_remote_state = "${var.env_remote_state}"
 
-#  env_remote_state    = "${var.env_remote_state}"
+  az_count = "${var.az_count}"
 
-#  az_count            = "${var.az_count}"
+  app_name = "${var.app_name}"
 
-#  app_name            = "${var.app_name}"
+  service_name = "dns"
 
-#  service_name        = "dns"
+  service_nets = ["${split(" ",data.consul_keys.app.var.dns_nets)}"]
 
-#  service_nets        = ["${split(" ",data.consul_keys.app.var.dns_nets)}"]
+  security_groups = ["${module.app.app_sg}"]
 
-#  security_groups     = ["${module.app.app_sg}"]
+  instance_type = ["${var.instance_type}"]
 
-#  instance_type       = ["${var.instance_type}"]
-
-#  user_data           = "${var.user_data}"
-
-#}
+  user_data = "${var.user_data}"
+}
 
 module "ntp" {
   source              = "../../fogg/service"
