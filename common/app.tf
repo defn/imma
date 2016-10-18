5935b91665d5990072fa7f6900f830dd31ba6b0b
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-18 02:16:52 -0700

    get rid of env stubs

--- common/app.tf
+++ common/app.tf
@@ -1,3 +1,19 @@
+variable global_remote_state {
+  default = ".global.tfstate"
+}
+
+variable env_remote_state {
+  default = ".env.tfstate"
+}
+
+data "terraform_remote_state" "env" {
+  backend = "local"
+
+  config {
+    path = "${var.env_remote_state}"
+  }
+}
+
 module "app" {
   source              = "../../module/app"
   global_remote_state = "${var.global_remote_state}"
--- common/sandbox.tf
+++
@@ -1,9 +0,0 @@
-variable sandbox_remote_state {}
-
-data "terraform_remote_state" "sandbox" {
-  backend = "local"
-
-  config {
-    path = "${var.sandbox_remote_state}"
-  }
-}
