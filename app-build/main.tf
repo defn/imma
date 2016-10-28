2ab541754a93db95be5536c2698c01d50da17a2c
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-27 20:57:58 -0700

    move everything to org/

--- app-build/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group_rule" "allow_build_mount" {
-  type                     = "ingress"
-  from_port                = 2049
-  to_port                  = 2049
-  protocol                 = "tcp"
-  source_security_group_id = "${module.app.app_sg}"
-  security_group_id        = "${data.terraform_remote_state.env.sg_efs}"
-}
--- env/global/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-domain_name = "imma.io"
-
-aws_region = "us-west-1"
-
-s3_remote_state = "imma-io-remote-state"
