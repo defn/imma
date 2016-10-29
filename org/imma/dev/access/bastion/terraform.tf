b45f33f60ff27aafd0f4255243bca26b02d59e26
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-29 03:23:56 -0700

    udp needs a return path

--- org/imma/dev/access/bastion/terraform.tf
+++ org/imma/dev/access/bastion/terraform.tf
@@ -34,3 +34,12 @@ resource "aws_security_group_rule" "allow_bastion_fan" {
   security_group_id        = "${data.terraform_remote_state.env.sg_env}"
 }
 
+resource "aws_security_group_rule" "allow_bastion_fan_return" {
+  type                     = "ingress"
+  from_port                = 8472
+  to_port                  = 8472
+  protocol                 = "udp"
+  source_security_group_id = "${data.terraform_remote_state.env.sg_env}"
+  security_group_id        = "${module.service.service_sg}"
+}
+
