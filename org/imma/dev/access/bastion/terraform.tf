9af4aa08b86b4211e6f40b86a616ede572a0f6f5
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-29 02:27:41 -0700

    adjust rules to bastion hosts

--- org/imma/dev/access/bastion/terraform.tf
+++ org/imma/dev/access/bastion/terraform.tf
@@ -4,7 +4,7 @@ resource "aws_security_group_rule" "bastion_ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${module.bastion.service_sg}"
+  security_group_id = "${module.service.service_sg}"
 }
 
 resource "aws_security_group_rule" "allow_bastion_ping" {
@@ -12,7 +12,7 @@ resource "aws_security_group_rule" "allow_bastion_ping" {
   from_port                = 9
   to_port                  = -1
   protocol                 = "icmp"
-  source_security_group_id = "${module.bastion.service_sg}"
+  source_security_group_id = "${module.service.service_sg}"
   security_group_id        = "${data.terraform_remote_state.env.sg_env}"
 }
 
@@ -21,6 +21,6 @@ resource "aws_security_group_rule" "allow_bastion_ssh" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = "${module.bastion.service_sg}"
+  source_security_group_id = "${module.service.service_sg}"
   security_group_id        = "${data.terraform_remote_state.env.sg_env}"
 }
